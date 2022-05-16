5e9918027441d2b8e223087c53f1df3e143d343d
JV-Albuquerque-S <alb.albqrq@gmail.com>
2022-05-16 16:53:49 -0300

    back carrinho

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -5,6 +5,5 @@ import { getProducts } from "../controllers/productsController.js";
 const productsRouter = Router();
 
 productsRouter.get("/products", getProducts);
-//userRouter.post("/user-cart", ...);
 
 export default productsRouter;
